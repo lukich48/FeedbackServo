d7973e91d6091b7907397807a8515f5d178c07ed
lukich48 <rodal2807@gmail.com>
2025-02-23 16:19:26 +0300

    init

---
+++ src/FeedbackServo.h
@@ -0,0 +1,186 @@
+#pragma once
+#include "arduino.h"
+
+#include <Servo.h>
+
+struct FeedbackInterval{
+    int minFeedback;
+    int maxFeedback;
+    int minAngle;
+    int maxAngle;
+};
+
+class FeedbackServo{
+    private:
+        Servo *servo;
+        void writeRange(int start, int end);
+        int anglePoints[10]; 
+        int feedbackPoints[10];
+        int pointsCount; 
+        void sortPoints();
+        FeedbackInterval getFeedbackInterval(int curFeedback);
+    public:
+        byte servoPin; 
+        byte feedbackPin;
+        int minFeedback;
+        int maxFeedback; 
+        int minAngle;
+        int maxAngle;
+        int minAllowedAngle;
+        int maxAllowedAngle;
+        int stopable = true;
+        FeedbackServo(Servo* servo, byte feedbackPin, int minFeedback, int maxFeedback, int minAngle = 0, int maxAngle = 180);
+        void addPoint(int angle, int feedback);
+        void setAllowedAngles(int minAllowedAngle, int maxAllowedAngle);
+        void write(int angle);
+};
+
+FeedbackServo::FeedbackServo(Servo* servo, byte feedbackPin, int minFeedback, int maxFeedback, int minAngle, int maxAngle)
+{
+    this->servoPin = servoPin;
+    this->feedbackPin = feedbackPin;
+    this->minFeedback = minFeedback;
+    this->maxFeedback = maxFeedback;
+    this->minAngle = minAngle;
+    this->maxAngle = maxAngle;
+    this->minAllowedAngle = minAngle;
+    this->maxAllowedAngle = maxAngle;
+
+    this->servo = servo;
+
+    anglePoints[pointsCount] = minAngle;
+    feedbackPoints[pointsCount] = minFeedback;
+    pointsCount++;
+
+    anglePoints[pointsCount] = maxAngle;
+    feedbackPoints[pointsCount] = maxFeedback;
+    pointsCount++;
+}
+
+void FeedbackServo::addPoint(int angle, int feedback){
+    anglePoints[pointsCount] = angle;
+    feedbackPoints[pointsCount] = feedback;
+    pointsCount++;
+
+    sortPoints();
+}
+
+void FeedbackServo::setAllowedAngles(int minAllowedAngle, int maxAllowedAngle){
+    this->minAllowedAngle = minAllowedAngle;
+    this->maxAllowedAngle = maxAllowedAngle;
+}
+
+void FeedbackServo::write(int angle){
+
+    // for (int i = 0; i < pointsCount; i++){
+    //     log_d("feedbackPoints: %d anglePoints: %d", feedbackPoints[i], anglePoints[i]);
+    // }
+    int feedback = analogRead(feedbackPin);
+    FeedbackInterval interval = getFeedbackInterval(feedback);
+    log_d("f: %d minf: %d maxf: %d mina %d maxa %d", feedback, interval.minFeedback, interval.maxFeedback, interval.minAngle, interval.maxAngle);
+    int curAngle = map(feedback, interval.minFeedback, interval.maxFeedback, interval.minAngle, interval.maxAngle);
+    log_d("curAngle: %d", curAngle);
+
+    writeRange(curAngle, angle);
+}
+
+void FeedbackServo::writeRange(int start, int end)
+{
+    servo->write(start);
+    delay(300);
+    int feedback = analogRead(feedbackPin);
+    log_d("%d: %d", start, feedback);
+
+    if (start < end){
+        for (int i = start+2; i<=end; i += 2){
+            servo->write(i);
+            delay(100);
+            int curFeedback = analogRead(feedbackPin);
+            log_d("%d: %d", i, curFeedback);
+            if (stopable && curFeedback < feedback){
+                i+= 2;
+                servo->write(i);
+                delay (300);
+                curFeedback = analogRead(feedbackPin);
+                log_d("%d: %d", i, curFeedback);
+                if(curFeedback < feedback){
+                    servo->write(i-3);
+                    break;
+                }
+            }
+            feedback = curFeedback;
+        }
+    }
+    else {
+        for (int i = start-2; i>=end; i -=2){
+            servo->write(i);
+            delay(100);
+            int curFeedback = analogRead(feedbackPin);
+            log_d("%d: %d", i, curFeedback);
+            if (stopable && curFeedback > feedback){
+                i-= 2;
+                servo->write(i);
+                delay (300);
+                curFeedback = analogRead(feedbackPin);
+                log_d("%d: %d", i, curFeedback);
+                if(curFeedback > feedback){
+                    servo->write(i+3);
+                    break;
+                }
+            }
+            feedback = curFeedback;
+        }
+    }
+}
+
+void FeedbackServo::sortPoints()
+{
+	int tempAngle = 0;
+	int tempFeedback = 0;
+	for (int i = 0; i < pointsCount; i++)
+	{
+		for (int j = i; j < pointsCount; j++)
+		{
+			if (anglePoints[i] > anglePoints[j])
+			{
+				tempAngle = anglePoints[i];
+				anglePoints[i] = anglePoints[j];
+				anglePoints[j] = tempAngle;
+
+                tempFeedback = feedbackPoints[i];
+                feedbackPoints[i] = feedbackPoints[j];
+				feedbackPoints[j] = tempFeedback;
+			}
+		}
+	}
+}
+
+FeedbackInterval FeedbackServo::getFeedbackInterval(int curFeedback){
+    FeedbackInterval res;
+
+    if(curFeedback <= feedbackPoints[0]){
+            res.minFeedback = curFeedback - 1;
+            res.maxFeedback = curFeedback + 1;
+            res.minAngle = anglePoints[0] - 1;
+            res.maxAngle = anglePoints[0] + 1;
+            return res;
+    }
+
+    if(curFeedback >= feedbackPoints[pointsCount-1]){
+        res.minFeedback = curFeedback - 1;
+        res.maxFeedback = curFeedback + 1;
+        res.minAngle = anglePoints[pointsCount-1] - 1;
+        res.maxAngle = anglePoints[pointsCount-1] + 1;
+        return res;
+    }
+
+    for (int i = 0; i < pointsCount; i++){
+        if(curFeedback < feedbackPoints[i]){
+            res.minFeedback = feedbackPoints[i-1];
+            res.maxFeedback = feedbackPoints[i];
+            res.minAngle = anglePoints[i-1];
+            res.maxAngle = anglePoints[i];
+            return res;
+        }
+    }
+}
---
+++ src/main.cpp
@@ -0,0 +1,125 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include <FeedbackServo.h>
+
+byte servoPin = 27;
+byte feedbackPin = 26;
+
+Servo servo;
+FeedbackServo feedbackServo(&servo, feedbackPin, 436, 4058, 0, 180);
+
+void writeRange(int start, int end, bool stopable)
+{
+    servo.write(start);
+    delay(2000);
+    int feedback = analogRead(feedbackPin);
+    Serial.printf("%d: %d\n", start, feedback);
+
+    if (start < end){
+        for (int i = start+1; i<=end; i += 1){
+            servo.write(i);
+            delay(200);
+            int curFeedback = analogRead(feedbackPin);
+            Serial.printf("%d: %d\n", i, curFeedback);
+            if (stopable && curFeedback < feedback){
+                i+= 2;
+                servo.write(i);
+                delay (300);
+                curFeedback = analogRead(feedbackPin);
+                Serial.printf("%d: %d\n", i, curFeedback);
+                if(curFeedback < feedback){
+                    servo.write(i-3);
+                    break;
+                }
+            }
+            feedback = curFeedback;
+        }
+    }
+    else {
+        for (int i = start-4; i>=end; i -=4){
+            servo.write(i);
+            delay(100);
+            int curFeedback = analogRead(feedbackPin);
+            Serial.printf("%d: %d\n", i, curFeedback);
+            if (stopable && curFeedback > feedback){
+                delay (200);
+                if(curFeedback > feedback){
+                    servo.write(i+1);
+                    break;
+                }
+            }
+            feedback = curFeedback;
+        }
+    }
+}
+
+void angleFromSerial(){
+    
+        int angle = Serial.parseInt();
+        Serial.printf("\nReceived: %d\n", angle);
+        
+        servo.write(angle);
+        int curFeedback = analogRead(feedbackPin);
+        Serial.printf("feedback: %d\n", curFeedback);
+
+}
+
+void rangeFromSerial(){
+    
+    char buf[32];
+    int amount = Serial.readBytesUntil('\n', buf, sizeof(buf));
+    Serial.printf("\nReceived: %s\n", buf);
+
+    int start, end, stopable;
+    sscanf(buf, "%d-%d %d", &start, &end, &stopable);
+    Serial.printf("start: %d end: %d stopable: %d\n", start, end, stopable);
+
+    if (stopable > 1)
+        stopable = 0;
+
+    writeRange(start, end, stopable);
+}
+
+void writeWithFeedbackFromSerial()
+{
+    int angle = Serial.parseInt();
+    Serial.printf("\nReceived: %d\n", angle);
+    
+    feedbackServo.write(angle);
+}
+
+void setup(){
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+
+    servo.attach(servoPin);
+    // servoR.attach(_servoRPin, Servo::CHANNEL_NOT_ATTACHED, 0, 180, 500, 2360);
+    feedbackServo.addPoint(30, 1045);
+    feedbackServo.addPoint(90, 2175);
+    feedbackServo.addPoint(163, 3554);
+}
+
+void loop(){
+    if (Serial.available() > 0) {
+
+        char cmd = Serial.read();
+
+
+        if (cmd == 'a'){
+            angleFromSerial();       
+        }
+        else if (cmd =='r'){
+            rangeFromSerial();
+        }
+        else if (cmd == 'f'){
+            writeWithFeedbackFromSerial();
+        }
+        
+        // if (cmd == 1){
+        //     writeFromStartToEnd(0, 180);
+        // }
+        // else if (cmd == 2){
+        //     writeFromEndToStart(0, 180);
+        // }
+    } 
+}
